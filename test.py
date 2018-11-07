d316e41392d38ce0971a64c0f16a5505a1730683
Kin <kina.siu@gmail.com>
2018-11-07 13:08:13 -0500

    created example scene, needs transitions

--- test.py
+++ test.py
@@ -1,26 +1,32 @@
+import inquirer
+current_scene = {}
+
 class Scene:
-    def __init__(self, desc, commands):
-        self.desc = desc
-        self.commands = commands
+    def __init__(self, description, action, command_list):
+        self.description = description
+        self.action = action
+        self.command_list = command_list
+        self.scene = [
+            inquirer.List('action',
+                message=self.action,
+                choices=self.command_list,
+            ),
+        ]
+        self.result = {}
 
-    def load_commands(self, commands):
-        for i in self.commands:
-            print(i)
-A = Scene('you are in room A', ['i','s','e'])
+penn_station = Scene('long ass descriptoin and stuff and things and more stuff and things and yes',
+"You are standing in Penn Station", ['Check MTA Schedule','eat a pizza','mike mike','charlie brown','turd nugget','jorge the great'])
+def start_scene(anything):
+    print(anything.description)
+    anything.result = inquirer.prompt(anything.scene)
 
-
+def set_scene(anything):
+    return anything.result
 #start trigger
+
+# start(penn_station.scene)
 if __name__ == '__main__':
-    current_scene = A
-    while current_scene == A:
-        cmd = input(A.desc)
-    #     cmd = test
-    #     if cmd.lower() not in ['i']:
-    #         print('sorry, I didn\'t get that')
-    #     else:
-    #         if cmd == 'i':
-    #             for x in player.inv:
-    #                 print(x)
-    #         if cmd == 't':
-    #             print(datetime.time())
-    #         if cmd == 'g':
+    print(current_scene)
+    start_scene(penn_station)
+    current_scene = set_scene(penn_station)
+    print(current_scene)
