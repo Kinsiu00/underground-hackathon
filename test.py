c30b328294c236088f441bc8938149a2fec3af88
Kin <kina.siu@gmail.com>
2018-11-08 10:25:51 -0500

    created initial transition/description test

--- test.py
+++ test.py
@@ -14,19 +14,34 @@ class Scene:
         ]
         self.result = {}
 
-penn_station = Scene('long ass descriptoin and stuff and things and more stuff and things and yes',
-"You are standing in Penn Station", ['Check MTA Schedule','eat a pizza','mike mike','charlie brown','turd nugget','jorge the great'])
+penn_station = Scene('long ass description and stuff and things and more stuff and things and yes',
+"You are standing in Penn Station", 
+['Check MTA Schedule','eat a pizza','mike mike','go to time\'s square','jorge the great'])
+times_square = Scene('there is a lot of billboards and people and a single naked cowboy',
+"where to, adventurer?",
+['tip the cowboy', 'get lost in the crowd', 'check out the lego store', 'find true love', 'go to penn station'])
+
 def start_scene(anything):
     print(anything.description)
     anything.result = inquirer.prompt(anything.scene)
 
 def set_scene(anything):
     return anything.result
+def set_description(anything, new_desc):
+    anything.description = new_desc
+    print(anything.description)
+    anything.result = inquirer.prompt(anything.scene)
+
 #start trigger
 
 # start(penn_station.scene)
 if __name__ == '__main__':
-    print(current_scene)
     start_scene(penn_station)
     current_scene = set_scene(penn_station)
-    print(current_scene)
+    if current_scene['action'] == 'go to time\'s square':
+        start_scene(times_square)
+    if current_scene['action'] == 'go to penn station':
+        start_scene(penn_station)
+    if current_scene['action'] == 'eat a pizza':
+        set_description(penn_station, 'you eat an overpriced slice of pizza, it is disappointing')
+        
