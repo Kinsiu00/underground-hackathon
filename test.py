3ead9baf071305e975a48d1d6bf723fc323ddf26
Kin <kina.siu@gmail.com>
2018-11-09 11:57:04 -0500

    incorporated scene transition method from R. - needs transition testing

--- test.py
+++ test.py
@@ -14,6 +14,17 @@ class Scene:
         ]
         self.action = {}
 
+class Action:
+    def __init__(self, action_dictionary, scene_trigger):
+        self.action_dictionary = action_dictionary
+        self.scene_trigger = scene_trigger
+    
+    def interaction(self, key):
+        print(self.action_dictionary[key])
+        if key == self.scene_trigger:
+            print('scene change!')
+
+
 
 # Different Scenes
 penn_station = Scene('long ass description and stuff and things and more stuff and things and yes',
@@ -24,27 +35,22 @@ times_square = Scene('there is a lot of billboards and people and a single naked
 "where to, adventurer?",
 ['tip the cowboy', 'get lost in the crowd', 'check out the lego store', 'find true love', 'go to penn station'])
 
+penn_station_actions = Action({'Check MTA Schedule':'late as usual', 'eat a pizza':'yum but pricy', 'mike mike':'mike mike mike', 'go to time\'s square':'off we go!', 'jorge the great':'is gone'}, 'go to time\'s square')
 
-def start_scene(element):
+def start_scene(element = penn_station):
     print(element.description)
     element.action = inquirer.prompt(element.scene)
 
 def set_description(element, new_desc):
     element.description = new_desc
-    # print(anything.description)
-    # anything.result = inquirer.prompt(anything.scene)
-
-
-def switch_scenes(arg):
-    switcher = {
-        "go to time's square": start_scene(times_square),
-        "go to penn station": start_scene(penn_station)
-    }
-    switcher.get(arg)
 
 #GAME START
-if __name__ == '__main__':
-    print(current_scene.action)
-    start_scene(current_scene)
-    while True:
-        switch_scenes(current_scene['action'])
+while __name__ == '__main__':
+    current_scene = penn_station
+    start_scene()
+
+    for key in penn_station_actions.action_dictionary:
+        if current_scene.action['action'] == key:
+            penn_station_actions.interaction(key)
+    if current_scene.action['action'] == 'go to penn station':
+        start_scene(penn_station)
